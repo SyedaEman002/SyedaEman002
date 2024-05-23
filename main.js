ac2e7f8966f7c001d072c4d4d96134ec288abaab
Syeda Eman <emansyeda189@gmail.com>
2024-05-23 22:42:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//Question no 1
+// let arrayV:(string)[]=["apple","banana","mango","orange"]
+// console.log(arrayV)
+// //Question no 2
+// let arrayC:(number)[]=[10,20,30,40]
+// console.log(arrayC)
+// //Question no 3
+// let arrayB:(string)[]=["10","15","20","30"]
+// console.log(arrayB,"arrayB before")
+// arrayB[2]="25"
+// console.log(arrayB,"arrayB after")
+// //Question no 4
+// let arrayS:(string)[]=["apple","mango","banana","orange"]
+// arrayS.push("grape")
+// console.log(arrayS)
+// //Question no 5
+// let arrayJ:(string)[]=["apple","mango","banana","orange","grape"]
+// arrayJ.pop()
+// console.log(arrayJ)
+// //Question no 6
+// let arrayT:(string)[]=["apple","mango","banana","orange","grape"]
+// arrayT.shift()
+// console.log(arrayT)
+// //Question no 7
+// let arrayI:(string)[]=["apple","banana","mango","orange","grape"]
+// arrayI.unshift("kiwi")
+// console.log(arrayI)
+// //Question no 8 
+// let arrayO:(string)[]=["apple","mango","banana","orange","grape"]
+// arrayO.shift()
+// arrayO.shift()
+// console.log(arrayO)
+// //Question no 9
+// let arrayP:(string)[]=["apple","mango","banana","grape"]
+// arrayP.push("pineapple")
+// arrayP.push("pear")
+// console.log(arrayP)
+
